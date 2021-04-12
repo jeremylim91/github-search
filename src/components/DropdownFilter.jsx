18d3341f810f59195bc977ee8cb69811b5e6d305
jeremylim91 <jeremylim_91@live.com>
2021-04-12 22:49:18 +0800

    created component using static variables for now

--- src/components/DropdownFilter.jsx
+++ src/components/DropdownFilter.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
+import {Dropdown, Row, Col} from 'react-bootstrap';
+import {queryTypeOptions} from '../utils';
 
 export default function DropdownFilter() {
-  return;
-  <></>;
+  const ManageFilterDropdown = (filter, dropdownOptions, relevantSetState) =>
+    dropdownOptions.map((elem) =>
+      elem !== filter ? (
+        <Dropdown.Item
+        // onClick={(e) => manageDropdownBtnClick(elem, relevantSetState)}
+        >
+          {elem}
+        </Dropdown.Item>
+      ) : (
+        ''
+      )
+    );
+
+  const queryType = 'Users';
+
+  return (
+    <Row>
+      <Col xs={2}>Query by:</Col>
+      <Col xs={2}>
+        <Dropdown>
+          <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
+            Name of filter
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {ManageFilterDropdown(
+              queryType,
+              queryTypeOptions /*, setQueryType*/
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Col>
+    </Row>
+  );
 }
