c7f3062260b9e956d7fc8dca8cd7d1d8ea8c5348
jeremylim91 <jeremylim_91@live.com>
2021-04-14 18:58:23 +0800

    added autocomplete using the autosuggest library and a sample list of users

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Form} from 'react-bootstrap';
+import Autosuggest from 'react-autosuggest';
+import sampleSuggestions from '../suggestions';
+export default function SearchInput(props) {
+  const {labelProp} = props;
+  // =========Set states=================
+  const [suggestions, setSuggestions] = useState(sampleSuggestions);
+  const [value, setValue] = useState('');
 
-export default function SearchInput() {
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  const getSuggestions = (value) => {
+    const inputValue = `${value}`.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0
+      ? []
+      : sampleSuggestions.filter(
+          (suggestion) =>
+            suggestion.name.toLowerCase().slice(0, inputLength) === inputValue
+        );
+  };
+
+  // When suggestion is clicked, Autosuggest needs to populate the input
+  // based on the clicked suggestion. Teach Autosuggest how to calculate the
+  // input value for every given suggestion.
+  const getSuggestionValue = (suggestion) => suggestion.name;
+
+  // Use your imagination to render suggestions.
+  const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
+
+  const onChange = (event, {newValue}) => {
+    setValue(newValue);
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  const onSuggestionsFetchRequested = ({value}) => {
+    setSuggestions(getSuggestions(value));
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+  console.log(`suggestions are:`);
+  console.log(suggestions);
+  console.log(`value is:`);
+  console.log(value);
+  // Autosuggest will pass through all these props to the input.
+  const inputProps = {
+    placeholder: 'Type a programming language',
+    value,
+    onChange,
+  };
   return (
     <div>
       <Form.Group>
         <Form.Label>{labelProp}:</Form.Label>
-        <ReactTags
-          suggestions={suggestionsProp}
-          tags={tagsProp}
-          onDelete={onDelete}
-          onAddition={onAddition}
-          noSuggestionsText="User not found; Please enter valid user"
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
         />
       </Form.Group>
     </div>
