3af7b94a3a42b8885ed940b5db22f361a6bce114
jeremylim91 <jeremylim_91@live.com>
2021-04-14 18:56:07 +0800

    renamed list to sampleSuggestions to prevent conflation with the state name in SearchInput component

---
+++ src/suggestions.mjs
@@ -0,0 +1,32 @@
+const sampleSuggestions = [
+{
+  name: 'Cane',
+  year: 1972,
+  },
+  {
+    name: 'Elm',
+    year: 2012,
+  },
+  {
+    name: 'Joseph',
+    year: 2012,
+  },
+  {
+    name: 'Jerome',
+    year: 2012,
+  },
+  {
+    name: 'Jack',
+    year: 2012,
+  },
+  {
+    name: 'James',
+    year: 2012,
+  },
+  {
+    name: 'June',
+    year: 2012,
+  },
+];
+
+export default sampleSuggestions
