9e66e44f6516d6da651f5d4edd3d7e4baa96f010
jeremylim91 <jeremylim_91@live.com>
2021-04-17 14:35:45 +0800

    added search with debounce to handle rate limit from github api

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,71 +1,117 @@
-import React, {useState, useEffect} from 'react';
-import {Form} from 'react-bootstrap';
+import React, {useState, useEffect, useCallback} from 'react';
+import {Form, Row, Col} from 'react-bootstrap';
+import {handleApiQuery} from '../utils';
 import Autosuggest from 'react-autosuggest';
-import sampleSuggestions from '../suggestions';
-export default function SearchInput(props) {
-  const {labelProp} = props;
+import debounce from 'lodash.debounce';
+
+export default function SearchInput({queryConfigs}) {
+  // destructure props
+  const {label, uri} = queryConfigs;
+
   // =========Set states=================
-  const [suggestions, setSuggestions] = useState(sampleSuggestions);
+  const [suggestions, setSuggestions] = useState('');
   const [value, setValue] = useState('');
 
-  // Teach Autosuggest how to calculate suggestions for any given input value.
-  const getSuggestions = (value) => {
-    const inputValue = `${value}`.trim().toLowerCase();
-    const inputLength = inputValue.length;
+  console.log(`suggestions is:`);
+  console.log(suggestions);
 
-    return inputLength === 0
-      ? []
-      : sampleSuggestions.filter(
-          (suggestion) =>
-            suggestion.name.toLowerCase().slice(0, inputLength) === inputValue
-        );
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  // const getSuggestions = (value) => {
+  //   console.log(`value in getSuggestions`);
+  //   console.log(value);
+  //   const inputValue = `${value}`.trim().toLowerCase();
+  //   const inputLength = inputValue.length;
+
+  //   return inputLength === 0
+  //     ? []
+  //     : suggestions.filter(
+  //         (suggestion) =>
+  //           suggestion.user.toLowerCase().slice(0, inputLength) === inputValue
+  //       );
+  // };
+
+  // ========API QUERY=============================
+  // Whenever value changes, make an api req that returns autocomplete suggestions based on the user input
+  // useEffect(() => {
+  //   handleApiQuery(value, setSuggestions);
+  // }, [value]);
+
+  //===============================================
+
+  const handleDropdownClick = (suggestionDetails) => {
+    window.location.href = `${suggestionDetails.html_url}`;
   };
 
+  // ================Autosuggest=======================
   // When suggestion is clicked, Autosuggest needs to populate the input
   // based on the clicked suggestion. Teach Autosuggest how to calculate the
   // input value for every given suggestion.
-  const getSuggestionValue = (suggestion) => suggestion.name;
+  const getSuggestionValue = (suggestion) => suggestion.login;
 
   // Use your imagination to render suggestions.
-  const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
+  const renderSuggestion = (suggestion) => (
+    <button
+      key={suggestion.id}
+      className="dropdown-options-btn"
+      onClick={() => handleDropdownClick(suggestion)}>
+      {suggestion.login}
+    </button>
+  );
 
   const onChange = (event, {newValue}) => {
     setValue(newValue);
   };
 
+  // strategy to solve rate limiter
+  /*
+  1. after first key stroke,trigger a countdown timer
+  2. if there are subsequent key strokes, reset this timer
+  3. when the timer hits 0, trigger an api request
+  */
+
   // Autosuggest will call this function every time you need to update suggestions.
   // You already implemented this logic above, so just use it.
-  const onSuggestionsFetchRequested = ({value}) => {
-    setSuggestions(getSuggestions(value));
+  // const debouncedVal = debounce(handleApiQuery(value, setSuggestions), 1000);
+  const debouncedVal = useCallback(
+    debounce((currValue) => handleApiQuery(currValue, setSuggestions), 500),
+    []
+  );
+
+  const onSuggestionsFetchRequested = ({value: currValue}) => {
+    console.log(`currValue is:`);
+    console.log(currValue);
+    // setSuggestions(getSuggestions(value));
+    debouncedVal(currValue);
   };
 
   // Autosuggest will call this function every time you need to clear suggestions.
   const onSuggestionsClearRequested = () => {
     setSuggestions([]);
   };
-  console.log(`suggestions are:`);
-  console.log(suggestions);
-  console.log(`value is:`);
-  console.log(value);
+
   // Autosuggest will pass through all these props to the input.
   const inputProps = {
-    placeholder: 'Type a programming language',
+    placeholder: 'Enter your query',
     value,
     onChange,
   };
+  //===============================================
   return (
-    <div>
-      <Form.Group>
-        <Form.Label>{labelProp}:</Form.Label>
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-        />
-      </Form.Group>
-    </div>
+    <Form.Group>
+      <Row>
+        <Col xs={12}>
+          <Form.Label>Search Github</Form.Label>
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+          />
+          <Form.Text className="formText"> {label}</Form.Text>
+        </Col>
+      </Row>
+    </Form.Group>
   );
 }
