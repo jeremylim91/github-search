a2d3bec0ade4f8300254e1c792b3357eff3ce670
jeremylim91 <jeremylim_91@live.com>
2021-04-17 14:35:01 +0800

    added lodash package

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,4 +1,11 @@
+import axios from 'axios';
+
 const QUERY_REPO = 'query repo';
 const QUERY_USER = 'query user';
 
 export const queryTypeOptions = [QUERY_REPO, QUERY_USER];
+
+const SEARCH_USERS_URI = 'https://api.github.com/search/users';
+const SEARCH_REPOS_URI = 'https://api.github.com/search/repo';
+
+export const handleApiQuery = () => {};
