ccd521121e3a986a545cf9e55adb3d5672196084
jeremylim91 <jeremylim_91@live.com>
2021-04-19 21:19:24 +0800

    added AdditionalInfoTable Component, as well as video to background of app

--- src/components/MainSearchPage.jsx
+++ src/components/MainSearchPage.jsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import React, {useState, useRef} from 'react';
+import {Container} from 'react-bootstrap';
 import DropdownFilter from './DropdownFilter';
+import AdditionalInfoTable from './AdditionalInfoTable';
 import SearchInput from './SearchInput';
 import {REPOS, USERS} from '../utils';
 
 export default function MainSearchPage() {
   // ===========useStates==================
   const [queryMode, setQueryMode] = useState(USERS);
-
+  const [tableData, setTableData] = useState([]);
   // based on the query mode, set the respective query URI and label (the lable prompts the user whether what to type in the input box)
   const getQueryConfigsAccordingToMode = (queryModes) => {
     switch (queryModes) {
@@ -27,18 +28,35 @@ export default function MainSearchPage() {
 
   return (
     <div className="wrapper-div">
-      {/* <video autoPlay loop muted className="background-video">
+      <video autoPlay loop muted className="background-video">
         <source
           // video credit: github
           src="https://github.githubassets.com/images/modules/site/home/globe-900.h264.mp4"
           type="video/mp4"
         />
-      </video> */}
-      <div className="container-wrapper">
-        <Container className="main-container margin-auto mt-4 p-4 ml-auto mr-auto">
-          <DropdownFilter queryMode={queryMode} setQueryMode={setQueryMode} />
-          <SearchInput queryConfigs={queryConfigsAccordingToMode} />
-        </Container>
+      </video>
+      {/* <div className="searchBar-wrapper-default"> */}
+      <Container
+        fluid
+        className={`${
+          tableData.length
+            ? 'main-container-with-table'
+            : 'main-container-default'
+        } margin-auto mb-0`}>
+        <DropdownFilter queryMode={queryMode} setQueryMode={setQueryMode} />
+        <SearchInput
+          queryConfigs={queryConfigsAccordingToMode}
+          setTableData={setTableData}
+        />
+      </Container>
+      {/* </div> */}
+      <div
+        className={`${
+          tableData.length ? 'table-wrapper' : 'table-wrapper-hidden'
+        }`}>
+        {tableData.length ? (
+          <AdditionalInfoTable tableData={tableData} />
+        ) : null}
       </div>
     </div>
   );
